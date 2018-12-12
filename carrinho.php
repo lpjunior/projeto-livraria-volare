6111985b0b2fc9035ada5bc9a1d351c63daf8681
Maria Sá <dud.franco11@gmail.com>
2018-12-12 21:47:13 -0200

    update carrinho.php

--- carrinho.php
+++ carrinho.php
@@ -22,13 +22,9 @@
                                 Nome do produto
                             </td>
                             <td>
-                                <div class="form-group"><!--tentar alinhar-->
-                                    <div class="row">
-                                        <button onclick="funcaoParaExecutarMenos()" type="button" class="btn btn-light" id="btnMenos">-</button>
-                                        <input type="text" class="form-control col-2 light" name="qtdProduto" min="1" id=quantidadeProduto>
-                                        <button onclick="funcaoParaExecutarMais()" type="button" class="btn btn-light" id="btnMais">+</button>
-                                    </div>
-                                </div>
+                                <button id="btnPlus" class="btn btn-light">+</button>
+                                <span id="qtdProd" class="badge badge-secondary" contenteditable>1</span>
+                                <button id="btnMenus" class="btn btn-light">-</button>
                             </td>
                             <td>R$ <span id="idpreco">100,00</span></td>
                             <td><!--fazer um js-->R$00,00</td>
@@ -39,78 +35,82 @@
             </div>
             <div class="col-4">
                 <div class="col bordasc">
-                    <h4 class="text-center">Resumo do pedido</h4>
-                        <p>
-                            Subtotal:<br/>
-                            Frete:<br/>
-                            Total:<br/>
-                        </p>
+                    <h4 class="text-center mt-3">Resumo do pedido</h4>
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                            <th scope="row">Subtotal</th>
+                            <td>R$<span id="idSubtotal">00,00</span></td>
+                            </tr>
+                            <tr>
+                            <th scope="row">Frete</th>
+                            <td>R$<span id="idFrete">00,00</span></td>
+                            </tr>
+                            <tr>
+                            <th scope="row" class="total">Total</th>
+                            <td class="total">R$<span id="idTotal">00,00</span></td>
+                            </tr>
+                        </tbody>
+                        </table>
                 </div>
-                <div class="col bordasc">
-                    <h5>Calcule o frete</h5>
+                <div class="col bordasc mt-3">
+                    <h5 class="text-center mt-3">Calcule o frete</h5>
 
                 </div>
             </div>
         </div>
     </div>
-
+    <div class="form-group"><!--tentar alinhar-->
+                                    <div class="row">
+                                        <button onclick="funcaoParaExecutarMenos()" type="button" class="btn btn-light" id="btnMenos">-</button>
+                                        <input type="text" class="form-control col-2 light" name="qtdProduto" min="1" id=quantidadeProduto>
+                                        <button onclick="funcaoParaExecutarMais()" type="button" class="btn btn-light" id="btnMais">+</button>
+                                    </div>
+                                </div>
 <?php require_once("footer.php"); ?>
 <script>
-    console.log(document.getElementById("idpreco").textContent);
-    console.log($("#idpreco").text());
-</script>
-<script>
 
+    $(function(){
+        $("#qtdProd").keyup(function() {
+            $qtd = parseInt($("#qtdProd").html());
+            if($qtd >= 99) {
+                $("#qtdProd").html(99);
+            } else if($qtd <= 0) {
+                $("#qtdProd").html(0);
+            }
+        });
+    });
 
+    $(function(){
+        $("#qtdProd").keyup(function() {
+            $qtd = parseInt($("#qtdProd").html());
+            if($qtd == "") {
+                $("#qtdProd").html(0);
+            } else if(isNaN($qtd)) {
+                $("#qtdProd").html(0);
+            }
+        });
+    });
 
-      document.getElementById("quantidadeProduto").addEventListener("keyup", function () {
-          if (isNaN(document.getElementById("quantidadeProduto").value)){
-          document.getElementById("quantidadeProduto").value = 0;
-        }
-      });
+    $(function(){
+        $("#btnPlus").click(function() {
+            $qtd = parseInt($("#qtdProd").html());
+            if($qtd >= 99) {
+                $("#qtdProd").html(99);
+            } else {
+                $("#qtdProd").html(++$qtd);
+            }
+        });
+    });
 
-    if(document.getElementById("quantidadeProduto").value == ""){
-      document.getElementById("quantidadeProduto").value = parseInt('1');
-   }
-
-    function funcaoParaExecutarMenos (){
-        quantProdutoo = document.getElementById("quantidadeProduto").value;
-        quantProduto = parseInt(quantProdutoo);
-        if (document.getElementById("quantidadeProduto").value <= 0){
-          document.getElementById("quantidadeProduto").value = parseInt('0');
-          exit;
-        }
-        if (document.getElementById("quantidadeProduto").value > 99){
-          document.getElementById("quantidadeProduto").value = parseInt('99');
-          exit;
-        }
-        quantProduto = quantProduto - 1;
-
-          document.getElementById("quantidadeProduto").value = (quantProduto);
-    }
-    function funcaoParaExecutarMais (){
-      quantProdutoo = document.getElementById("quantidadeProduto").value;
-      quantProduto = parseInt(quantProdutoo);
-      if (document.getElementById("quantidadeProduto").value == ""){
-      document.getElementById("quantidadeProduto").value = 1;
-      exit;
-
-      }
-      if (document.getElementById("quantidadeProduto").value < 0){
-        document.getElementById("quantidadeProduto").value = parseInt('0');
-      }
-      if (document.getElementById("quantidadeProduto").value >= 99){
-        document.getElementById("quantidadeProduto").value = parseInt('99');
-        exit;
-      }
-      quantProduto = quantProduto + 1;
-
-        document.getElementById("quantidadeProduto").value = (quantProduto);
-    }
-
-    document.getElementById("quantidadeProduto").addEventListener("keyup", function () {
-        if (document.getElementById("quantidadeProduto").value >= 99){
-        document.getElementById("quantidadeProduto").value = 99;
-      }
+    $(function(){
+        $("#btnMenus").click(function() {
+            $qtd = parseInt($("#qtdProd").html());
+            if($qtd <= 0) {
+                $("#qtdProd").html(0);
+            } else {
+                $("#qtdProd").html(--$qtd);
+            }
+        });
     });
 </script>
