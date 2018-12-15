25878488e5e60ab85364d4647ee413bee389bf50
Nathália <44986577+cielspride@users.noreply.github.com>
2018-12-15 14:12:17 -0200

    criação pag

---
+++ paginasnovas/checkout.php
@@ -0,0 +1,35 @@
+<?php require_once("header.php");?
+    <section class="container col-xs-12 col-sm-12 col-md-8 col-lg-8">
+      <div class="row">
+          <div class="col-md-6 col-lg-6">
+              <p>integração pag seguro</p>
+          </div>
+
+          <div class="col-md-6 col-lg-6 bordasb">
+              <h1 class="fontevinte">Sua compra</h1>
+              <h3 class="fontedezoito"><b>Subtotal</b></h3>
+              <!-- produtos do carrinho, total e preço do total de exemplares -->
+              <table class="table table-sm">
+                  <tbody>
+                      <tr>
+                        <th scope="row">Produtos:</th>
+                        <td>produto x</td>
+                        <td>produto y</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">Subtotal</th>
+                        <td>preço do produto x . quantidade</td>
+                        <td>preço do produto y . quantidade</td>
+                      </tr>
+                      <tr>
+                        <th scope="row">3</th>
+                        <td colspan="2">Larry the Bird</td>
+                        <td>@twitter</td>
+                      </tr>
+                    </tbody>
+              </table>
+              <!-- fim produtos do carrinho -->
+          </div>
+      </div>
+      </section>
+<?php require_once("footer.php");?
