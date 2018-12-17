f3bb3f9055eb0eea19c7f13f88f67e0da6eca8da
cielspride <nathalia_fonseca@id.uff.br>
2018-12-17 14:14:13 -0200

    link banner 3

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ $app->get('/home', function ($request, $response, $args) {?>
                         <img class="d-block w-100" src="img/banner2.jpg" alt="Second slide">
                       </div>
                       <div class="carousel-item">
-                        <img class="d-block w-100" src="img/banner1.jpg" alt="Third slide">
+                        <img class="d-block w-100" src="img/banner3.jpg" alt="Third slide">
                       </div>
                     </div>
                     <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
