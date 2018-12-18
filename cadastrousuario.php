58aa7016ce09f76975ea89aa0f38c47d146f43b0
Mefs11 <44986674+Mefs11@users.noreply.github.com>
2018-12-18 12:42:23 -0200

    Update cadastrousuario.php

--- cadastrousuario.php
+++ cadastrousuario.php
@@ -1,30 +1,30 @@
 <?php
 $app->get('/cadastro', function ($request, $response, $args) {
 ?>
-            <div class="container-fluid col-md-8 centraliza mt-4">
+            <div class="container-fluid col-md-10 centraliza mt-4">
                 <fieldset><!-- *************início do formulário ********************** -->
                     <legend><h1 class="fontevinteecinco">Cadastro</h1></legend>
                     <form action="php/CRUDS/registroUsuario.php" method="POST">
                         <div class="form-group"> <h4 class="fontedezoito"><b>Dados pessoais</b></h4>
 							<div class="row">
-								<div class="col">
+								<div class="col-md">
 									<label for="iNome">Nome:</label>
 									<input type="text" id="iNome" name="txtNome" class="form-control" maxlength="100" required>
 									<br/>
 								</div>
-								<div class="col">
+								<div class="col-md">
 									<label for="iSobrenome">Sobrenome:</label>
 									<input type="text" id="iSobrenome" name="txtSobrenome" class="form-control" maxlength="80" required>
 									<br/>
 								</div>
 							</div>
 							<div class="row">
-								<div class="col">
+								<div class="col-md">
 									<label for="iDataNasc">Data de nascimento:</label>
 									<input type="text" id="iDataNasc" name="txtDataNasc" class="form-control date" required>
 									<br/>
 								</div>
-								<div class="col">
+								<div class="col-md">
 									<label for="sGenero">Sexo</label>
 									<select class="form-control" id="sGenero" name="txtGenero" required>
 										<option value="2">Feminino</option>
@@ -33,28 +33,28 @@ $app->get('/cadastro', function ($request, $response, $args) {
 								</div>
 							</div>
 							<div class="row">
-								<div class="col">
+								<div class="col-md">
 									<label for="iCPF">CPF:</label>
 									<input type="text" id="iCPF" name="txtCPF" class="form-control cpf" required>
 									<br/>
 								</div>
-								<div class="col">
+								<div class="col-md">
 									<label for="iTelefone">Telefone:</label>
 									<input type="text" id="i" name="txt" class="form-control phone" required>
 									<br/>
 								</div>
 							</div>
 							<div class="row">
-								<div class="col">
+								<div class="col-md">
 									<label for="iEmail">E-mail:</label>
 									<input type="email" id="iEmail" name="txtEmail" class="form-control" maxlength="100" required>
 									<br/>
 								</div>
-								<div class="col">
+								<div class="col-md">
 									<label for="isenha">Senha:</label><!--orientações para senha? número e tipo de caracter-->
 									<input type="password" class="form-control" id="isenha" name="isenha" required>
 								</div>
-								<div class="col">
+								<div class="col-md">
 									<label for="isenha2">Confirmação de Senha:</label><!--script para confirmação de senha-->
 									<input type="password" class="form-control" id="isenha2" name="isenha2"  required>
 								</div>
@@ -63,36 +63,36 @@ $app->get('/cadastro', function ($request, $response, $args) {
                         <br/>
                         <div class="form-group"><h4 class="fontedezoito"><b>Endereço de cobrança</b></h4><br/><!-- mudar estilo do texto-->
                             <div class="row">
-								<div class="col-md-2"><!--adicionar tipo de coluna, testar layout-->
+								<div class="col-md-4">
 									<label for="iCEP">CEP:</label>
 									<input type="text" id="iCEP" name="txtCEP" class="form-control cep" required>
 								</div>
-								<div class="col"><!--adicionar tipo de coluna, testar layout-->
+								<div class="col-md"><!--adicionar tipo de coluna, testar layout-->
 									<label for="iEndCobr">Endereço:</label>
 									<input type="text" id="iEndCobr" name="txtEndCobr" class="form-control" required maxlength="255">
 								</div>
 							</div>
 							<div class="row">
-								<div class="col">
+								<div class="col-md">
 									<label for="iNum">Número:</label>
 									<input type="text"  id="iNum" name="txtNum" class="form-control" required maxlength="10">
 								</div>
-								<div class="col">
+								<div class="col-md">
 									<label for="iComplemento">Complemento:</label>
 									<input type="text"  id="iComplemento" name="txtComplemento" class="form-control" required maxlength="15">
 									<br/>
 								</div>
-								<div class="col">
+								<div class="col-md">
 									<label for="iBairro">Bairro:</label>
 									<input type="text"  id="iBairro" name="txtBairro" class="form-control" required maxlength="50">
 								</div>
 							</div>
 							<div class="row">
-								<div class="col">
+								<div class="col-md">
 									<label for="iCidade">Cidade:</label>
 									<input type="text"  id="iCidade" name="txtCidade" class="form-control" required maxlength="50">
 								</div>
-								<div class="col">
+								<div class="col-md">
 									<label for="sEstado">Estado</label><!-- PESSOAL DO PHP: tem que puxar esse select do banco de dados, só coloquei pra ficar mais fácil de vizualizar-->
 									<select id="sEstado" name="txtEstado" class="form-control">
 										<option value="AC">Acre</option>
@@ -125,9 +125,17 @@ $app->get('/cadastro', function ($request, $response, $args) {
 									</select>
 								</div>
 							</div>
-							<br/>
+							<div class="row pt-4 pl-3">
+								<label for="genFav">Interesses</label>
+								<div id="genFav" class="col-md ">
+									<div class="form-check form-check-inline">
+										<input class="form-check-input" type="checkbox" name="" id="cb1" value="option1">
+										<label class="form-check-label" for="cb1">Puxar do banco</label>
+									</div>
+								</div>
+							</div>
 							<div class="row">
-							<div class="col mb-4">
+							<div class="col mb-4 mt-4">
                             		<button type="submit" class="btn COLORE1" name="btn-enviar" >Criar sua conta</button><!--value??-->
                         		</div>
 							</div>
