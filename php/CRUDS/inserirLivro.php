ae1eeaed882d8b076755f37f3dd2b28bf4912a6b
Roberto Pereira Marques <matsufrg@gmail.com>
2018-12-19 20:45:20 -0200

    haha

--- php/CRUDS/inserirLivro.php
+++ php/CRUDS/inserirLivro.php
@@ -2,11 +2,12 @@
 session_start();
 require_once 'serviceBook.php';
 	if (isset($_POST['livro-enviar'])) {
-			if ($livro = serviceInserir($_POST['categoria'], $_POST['titulo'], $_POST['autor'], $_POST['editora'],
+			if ($livro = serviceInserir(
+				$_POST['categoria'], $_POST['titulo'], $_POST['autor'], $_POST['editora'],
 				$_POST['ISBN'], $_POST['numeropaginas'], $_POST['sinopse'], $_POST['peso'],
-				$_POST['data_publicacao'], $_POST['fornecedor'], $_POST['preco'], $_POST['subcategorias'],
-				$_POST['capa'])){
-
+				$_POST['data_publicacao'], $_POST['fornecedor'], $_POST['preco'],
+				$_POST['subcategorias'], $_POST['capa'], $_POST['dimensoes'], $_POST['quantidade'],
+				$_POST['idiomas'], $_POST['sinopse'])){
 				echo $livro;
 			} else {
 				echo $livro;
