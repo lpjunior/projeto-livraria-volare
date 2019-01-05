055bfdee7a309d35e97cf469eb440bfc5f877f70
Roberto <44986570+matsufrg@users.noreply.github.com>
2019-01-05 17:58:55 -0200

    Update logout.php

--- adm/logout.php
+++ adm/logout.php
@@ -1,10 +1,10 @@
 <?php
 // Só poder entrar quando logado
-if (isset($_SESSION['user'])){
 session_start();
+if (isset($_SESSION['user'])){
 session_unset();
 session_destroy();
 header('Location: adm.php');
 } else {
-  header('location: adm.php');
+  header('Location: adm.php');
 }
